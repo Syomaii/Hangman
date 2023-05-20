c54baafffeb5ed9a8a5068f5171107eb9783f533
Syomai <126589688+Syomaii@users.noreply.github.com>
2023-05-20 17:42:21 +0800

    Add files via upload

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblGuesses = new System.Windows.Forms.Label();
+            this.txtBoxGuessWord = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HANGMAN GAME";
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(236, 103);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(64, 25);
+            this.lblWord.TabIndex = 1;
+            this.lblWord.Text = "label2";
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(221, 234);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(100, 41);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblGuesses
+            // 
+            this.lblGuesses.AutoSize = true;
+            this.lblGuesses.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuesses.Location = new System.Drawing.Point(105, 292);
+            this.lblGuesses.Name = "lblGuesses";
+            this.lblGuesses.Size = new System.Drawing.Size(64, 25);
+            this.lblGuesses.TabIndex = 3;
+            this.lblGuesses.Text = "label3";
+            // 
+            // txtBoxGuessWord
+            // 
+            this.txtBoxGuessWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxGuessWord.Location = new System.Drawing.Point(194, 165);
+            this.txtBoxGuessWord.Name = "txtBoxGuessWord";
+            this.txtBoxGuessWord.Size = new System.Drawing.Size(151, 28);
+            this.txtBoxGuessWord.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 361);
+            this.Controls.Add(this.txtBoxGuessWord);
+            this.Controls.Add(this.lblGuesses);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblGuesses;
+        private System.Windows.Forms.TextBox txtBoxGuessWord;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        private const int MaxGuess = 6;
+        private string word;
+        private char[] gword;
+        private int wrongGuess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            char letter = txtBoxGuessWord.Text[0];
+            txtBoxGuessWord.Text = "";
+
+            bool RightAns = false;
+            for(int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == letter)
+                {
+                    gword[i] = letter;
+                    RightAns = true;
+                }
+            }
+            if (RightAns)
+            {
+                if(new string(gword) == word)
+                {
+                    MessageBox.Show("YOU WIN!!!");
+                }    
+            }
+            else
+            {
+                wrongGuess++;
+                if(wrongGuess >= MaxGuess)
+                {
+                    MessageBox.Show($"You failed, the word is {word}");
+                }
+            }
+            UpGuesses();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            word = lblWord.Text.ToLower();
+            word = "Banana";
+            gword = new char[word.Length];
+            for(int i = 0; i<gword.Length; i++)
+            {
+                gword[i] = '*';
+            }
+            wrongGuess = 0;
+            UpGuesses();
+        }
+
+        private void UpGuesses()
+        {
+            lblWord.Text = new string(gword);
+            lblGuesses.Text = $"Incorrect guesses: {wrongGuess}/{MaxGuess}";
+        }
+    }
+}
